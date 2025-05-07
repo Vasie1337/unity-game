5caa490b3ae8db9fdff6fa2d2344efaa21a2d6b3
Vasie <59412971+Vasie1337@users.noreply.github.com>
2025-05-07 14:48:47 +0200

    Added level mng on main ui

--- Assets/Scripts/Game/UI/MainMenuMng.cs
+++ Assets/Scripts/Game/UI/MainMenuMng.cs
@@ -1,6 +1,33 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuMng : MonoBehaviour
 {
-    
+    [Header("Main Menu UI")]
+    public Button[] levelButtons;
+    public Button exitBtn;
+
+    public void LoadLevel(int levelIndex)
+    {
+        Debug.Log($"Level {levelIndex + 1} clicked");
+        SceneManager.LoadScene($"Level{levelIndex + 1}");
+    }
+
+    public void OnClickExit()
+    {
+        Debug.Log("Exit clicked");
+        Application.Quit();
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            int capturedIndex = i;
+            levelButtons[i].onClick.AddListener(() => LoadLevel(capturedIndex));
+        }
+
+        exitBtn.onClick.AddListener(OnClickExit);
+    }
 }
