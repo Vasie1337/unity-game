d9cb17db5d354dbaedbf0448a1d47beb34337db3
Vasie <59412971+Vasie1337@users.noreply.github.com>
2025-05-14 13:32:11 +0200

    Type shit

--- Assets/Scripts/Game/Core/GameManager.cs
+++ Assets/Scripts/Game/Core/GameManager.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+        }
+    }
 }
